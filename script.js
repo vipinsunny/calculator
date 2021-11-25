27682ea25567ef5d6df636514064a5b8f094d619
vipinsunny <vipinsunny96@gmail.com>
2021-11-25 16:37:51 +0530

    first

--- script.js
+++ script.js
@@ -11,12 +11,24 @@ var clearBox=()=>result.value=""
 //     let res=document.querySelector("#result")
 //     let cur_value=res.value
 //     let result=eval(cur_val)
-//     res.value=result.
+//     res.value=result.value
 
 // }
 var evaluateExpression=()=>result.value=eval(result.value)
-function backSpace(){
-    let res=document.querySelector("#result")
-    let cur_value=res.value
-    res.value=cur_value.slice(0, -1)
-}
+// function backSpace(){
+//     let res=document.querySelector("#result")
+//     let cur_value=res.value
+//     res.value=cur_value.slice(0, -1)
+// }
+var backSpace=()=>result.value=result.value.slice(0, -1)
+
+//account number=> text box
+// email=>text
+// phone=>
+// password=>
+// button
+
+
+
+// accnt no
+// password
